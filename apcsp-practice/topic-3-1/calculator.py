a16bc0920aa6bc9f80383a0a7e85c424685dda63
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-13 02:18:21 +0000

    redo calculator

--- apcsp-practice/topic-3-1/calculator.py
+++ apcsp-practice/topic-3-1/calculator.py
@@ -1,7 +1,9 @@
+money_avaliable =  int(input("How much money do you have: "))
+cases_buying = int(input("How many cases are you buying: "))
+case_cost = int(input("How much do the cases cost: "))
 
+total_cost = cases_buying * case_cost
+money_left = money_avaliable - total_cost
 
-money_avaliable=int(input("Money owned: ")) #input
-cases_bought=int(input("Cases bought: "))   #input
-cost_per_case=int(input("Price of case: "))  #input
-money_left=money_avaliable-cases_bought*cost_per_case
-print("Remaining money: ", money_left)  #output
+print("Total cost of cases: " + str(total_cost))
+print("Money left after buying cases: " + str(money_left))
