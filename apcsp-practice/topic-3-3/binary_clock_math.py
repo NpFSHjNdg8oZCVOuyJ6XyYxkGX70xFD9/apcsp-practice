a3d70e6a9ed6f1deea4ee02dca7ff8749b0b3721
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-14 05:56:16 +0000

    Improve topic-3-3 to achieve level 4

--- apcsp-practice/topic-3-3/binary_clock_math.py
+++ apcsp-practice/topic-3-3/binary_clock_math.py
@@ -1,24 +1,8 @@
-clock_value=45
-remaining = clock_value
-
-bit_1=remaining %2
-remaining=remaining//2
-bit_2=remaining %2
-remaining=remaining//2
-bit_4=remaining %2
-remaining=remaining//2
-bit_8=remaining %2
-remaining=remaining//2
-bit_16=remaining %2
-remaining=remaining//2
-bit_32=remaining %2
-remaining=remaining//2
-
 clock_values=[13,42]
-labels=["hours","minutes"]
+labels=["Hours","Minutes"]
 clock_values.append(17)
-labels.append("seconds")
-selected_index=0
+labels.append("Seconds")
+selected_index=2 # using the same index keeps the value and label together so its easier to add more
 clock_value=clock_values[selected_index]
 label=labels[selected_index]
 
@@ -39,6 +23,5 @@ remaining=remaining//2
 bits=[bit_32,bit_16,bit_8,bit_4,bit_2,bit_1]
 bit_text = str(bits[0]) + str(bits[1]) + str(bits[2]) + str(bits[3]) + str(bits[4]) + str(bits[5])
 check_value = bits[0] * 32 + bits[1] * 16 + bits[2] * 8 + bits[3] * 4 + bits[4] * 2 + bits[5] * 1
-print(str(label) + " : " + str(check_value) + " --> " + str(bit_text))
-print("Original : ", clock_value )
-print("Reconstructed: ", check_value)
+print(str(label) + ": " + str(clock_value) + " --> " + str(bit_text))
+print("Original: ", str(clock_value) + ", " + "Reconstructed: " + str(check_value))
