2d3ece9b51a1ab76bccc577dded1958320e26430
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-16 00:24:03 +0000

    Complete steps 1 through 4 topic-3-7

--- apcsp-practice/topic-3-7/sorting_station.py
+++ apcsp-practice/topic-3-7/sorting_station.py
@@ -6,5 +6,17 @@ size = int(label[7:10])
 weight = int(label[10:14])
 condition = label[14]
 
-if condition == "D" or size >= 050 or weight >= 2000: 
+if condition == "D" or size >= 50 or weight >= 2000: 
     print(label, "Sent to INSPECT group")
+
+elif color == "RED" and shape == "BALL" and size >= 10:
+    print(label, "Sent to group B")
+
+elif shape == "BALL":
+    print(label, "Sent to group A")
+
+elif shape == "CUBE" and (color == "BLU" or "GRN") and size <= 10:
+    print(label, "Sent to group C")
+
+else:
+    print("Invalid label")
