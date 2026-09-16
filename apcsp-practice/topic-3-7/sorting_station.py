c87c460d71b6c0da026696c65e39df9037c41549
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-16 00:13:35 +0000

    Add first sort and change ending to condition topic-3-7

--- apcsp-practice/topic-3-7/sorting_station.py
+++ apcsp-practice/topic-3-7/sorting_station.py
@@ -4,5 +4,7 @@ shape = label[0:4]
 color = label[4:7]
 size = int(label[7:10])
 weight = int(label[10:14])
-ending = label[14]
+condition = label[14]
 
+if condition == "D" or size >= 050 or weight >= 2000: 
+    print(label, "Sent to INSPECT group")
