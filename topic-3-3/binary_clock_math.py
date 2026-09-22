319d080859725a37e2eacd42417bb8c6a6856a37
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-22 03:58:22 +0000

    Fix binary clock math topic-3-3

--- topic-3-3/binary_clock_math.py
+++ topic-3-3/binary_clock_math.py
@@ -7,20 +7,27 @@ clock_value = clock_values[selected_index]
 label = labels[selected_index]
 
 remaining = clock_value
-bit_1 = remaining % 2
-remaining = remaining // 2
-bit_2 = remaining % 2
-remaining = remaining // 2
-bit_4 = remaining % 2
-remaining = remaining// 2
-bit_8 = remaining % 2
-remaining = remaining //2
-bit_16 = remaining % 2
-remaining = remaining// 2
-bit_32 = remaining % 2
-remaining = remaining// 2
+bits = []
 
-bits=[bit_32,bit_16,bit_8,bit_4,bit_2,bit_1]
+bits.append(remaining % 2)
+remaining = remaining // 2
+
+bits.append(remaining % 2)
+remaining = remaining // 2
+
+bits.append(remaining % 2)
+remaining = remaining // 2
+
+bits.append(remaining % 2)
+remaining = remaining // 2
+
+bits.append(remaining % 2)
+remaining = remaining // 2
+
+bits.append(remaining % 2)
+
+bits.reverse()
+
 bit_text = str(bits[0]) + str(bits[1]) + str(bits[2]) + str(bits[3]) + str(bits[4]) + str(bits[5])
 check_value = bits[0] * 32 + bits[1] * 16 + bits[2] * 8 + bits[3] * 4 + bits[4] * 2 + bits[5] * 1
 print("Selected index: " + str(selected_index))
